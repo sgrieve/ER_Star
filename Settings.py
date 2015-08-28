aff045431cd103f722ad6f35dc3e67a9934d9510
Stuart Grieve <s.grieve@ed.ac.uk>
2015-08-28 16:19:45 +0100

    First commit of settings file

---
+++ Settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# params to load the data
+Path = 2#'C:\\Users\\Stuart\\Dropbox\\data\\final\\' #must be wrapped in quotes with a trailing slash eg 'home/user/data/'
+Prefix = 'GM'
+Order = 2
+
+#data to plot
+RawFlag = 0
+DensityFlag = 0
+BinFlag = ''
+NumBins = 20
+PatchFlag = 0
+BasinFlag = 1
+LandscapeFlag = 0
+
+# how Sc is constrained
+Sc_Method = 'patches'
+ForceSc = False
+
+#plot style options
+ErrorBarFlag = True
+Format = 'png'
+
