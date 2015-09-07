32a7db993b303ee38435764b8dc495584d9f3ab5
Stuart Grieve <s.grieve@ed.ac.uk>
2015-09-07 11:58:58 +0100

    Adding comments to explain params.

--- Settings.py
+++ Settings.py
@@ -7,9 +7,13 @@ Order = 2 #Basin order used in ER_STAR.cpp to extract the drainage basins. eg 1,
 
 #Options to select data to be plotted
 RawFlag = 0 #Use 1 to plot the raw data and 0 to not plot it.
-DensityFlag = 0 #
-BinFlag = '' #
-NumBins = 20 #Number of bins to be generated. Must be an integer. eg 5,11,20
+DensityFlag = 0 #Use 0 to not plot the raw data as a density plot and 1 to plot 
+                #a density plot. Values greater than 1 will be used the thin the 
+                #data. For example 2 will plot every second point. Reccommended!
+BinFlag = '' #Use 'raw' to bin the raw data, 'patches' to bin the hilltop patch 
+             #data and an empty string, '' to not perform binning. 
+             #Note that quotes are needed for all cases.
+NumBins = 20 #Number of bins to be generated. Must be an integer. eg 5,11,20 Will be ignored if BinFlag is left blank.
 PatchFlag = 0 #Use 1 to plot the patch data and 0 to not plot it.
 BasinFlag = 1 #Use 1 to plot the basin data and 0 to not plot it.
 LandscapeFlag = 0 #Use 1 to plot the landscape average data and 0 to not plot it.
