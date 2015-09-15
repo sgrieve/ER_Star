7c0e578552338a1e5083a01021c6f8bb77052ae4
Stuart Grieve <s.grieve@ed.ac.uk>
2015-09-15 12:35:21 +0100

    Settings updated

--- Settings.py
+++ Settings.py
@@ -2,7 +2,7 @@
 
 # Parameters to load the data
 Path = 'C:\\Users\\Stuart\\Dropbox\\data\\final\\' #Must be wrapped in quotes with a trailing slash eg 'home/user/data/'
-Prefix = 'OR' #Must be wrapped in quotes and match the prefix used in ER_STAR.cpp
+Prefix = 'GM' #Must be wrapped in quotes and match the prefix used in ER_STAR.cpp
 Order = 2 #Basin order used in ER_STAR.cpp to extract the drainage basins. eg 1,2,5
 
 #Options to select data to be plotted
@@ -14,7 +14,7 @@ BinFlag = '' #Use 'raw' to bin the raw data, 'patches' to bin the hilltop patch
              #data and an empty string, '' to not perform binning. 
              #Note that quotes are needed for all cases.
 NumBins = 20 #Number of bins to be generated. Must be an integer. eg 5,11,20 Will be ignored if BinFlag is left blank.
-PatchFlag = 0 #Use 1 to plot the patch data and 0 to not plot it.
+PatchFlag = 1 #Use 1 to plot the patch data and 0 to not plot it.
 BasinFlag = 1 #Use 1 to plot the basin data and 0 to not plot it.
 LandscapeFlag = 0 #Use 1 to plot the landscape average data and 0 to not plot it.
 
@@ -32,6 +32,6 @@ ErrorBarFlag = False #True to plot errorbars on datapoints, False to exclude the
 Format = 'png' #File format for the output E*R* plots. Must be one of: 'png','pdf','ps','eps','svg'
 
 #Comparison data to be plotted from the other studies
-GabilanMesa = False #True to plot the data, False to exclude it.
+GabilanMesa = True #True to plot the data, False to exclude it.
 OregonCoastRange = False #True to plot the data, False to exclude it.
 SierraNevada = False #True to plot the data, False to exclude it.
